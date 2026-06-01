23a1717415c8cba89c7dc2ca8143e781c26ee4b5
Brayden Freitas <baf57@pitt.edu>
2026-06-01 00:07:10 -0400

    adding debug to understand order of functions

--- sketch.js
+++ sketch.js
@@ -4,21 +4,24 @@ function setup() {
 
     // setup text drawing params
     textSize(32);
-    fill(255);
+   fill(255);
     stroke(0);
     strokeWeight(4);
     text("Click to begin animation", 100,100);
 
     // wait for click to animate
     noLoop();
+    console.log("Should not be looping now");
 }
 
 function draw() {
     //background(0);
+    console.log("looping");
     rotate(PI/60);
 }
 
 // begin animating when clicked
 function mouseClicked(){
+    console.log("begin loop");
     loop();
 }
