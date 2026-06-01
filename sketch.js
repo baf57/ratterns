2e6f3fbc3c57123fbcf3be24b570461fb7eeb81a
Brayden Freitas <andrewbrayden98@gmail.com>
2026-05-31 21:24:49 -0400

    add basic animation start functionality

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
 function setup() {
     createCanvas(800, 600);
+    background(0);
+
+    // setup text drawing params
+    textSize(32);
+    fill(255);
+    stroke(0);
+    strokeWeight(4);
+    text("Click to begin animation", 100,100);
+
+    // wait for click to animate
+    noLoop();
 }
 
 function draw() {
-    background(0);
+    //background(0);
+    rotate(PI/60);
+}
+
+// begin animating when clicked
+function mouseClicked(){
+    loop();
 }
