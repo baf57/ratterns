deaa9d09ba48e66533cae6d134906f0f62eb8e28
Brayden Freitas <baf57@pitt.edu>
2026-06-06 22:46:35 -0400

    fixed light/dark mode

--- js/colorSet.js
+++ js/colorSet.js
@@ -1,16 +1,14 @@
 // dark/light mode switch function
-function dlSwitch(){
+function darkModeToggle(){
+    console.log("toggling dark mode:");
     document.body.classList.toggle("dark-mode");
-    console.log("switching dark/light mode:");
-    console.log("   ",document.body.classList);
 }
 
-// default is light scheme - switch to dark at start if dark
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
-    dlSwitch();
-} else {
-    console.log("staying in light mode");
+if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+    console.log("setting dark mode");
+    document.body.classList.add("dark-mode");
 }
 
 // if the user switches off the dark scheme then return to light and vice versa
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', dlSwitch());
+window.matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', darkModeToggle);
