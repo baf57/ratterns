c50ac037ef0e56b97d32dc631a36ec1aa92894d3
Brayden Freitas <baf57@pitt.edu>
2026-06-07 22:29:32 -0400

    log -> debug level because annoying

--- js/colorSet.js
+++ js/colorSet.js
@@ -1,11 +1,11 @@
 // dark/light mode switch function
 function darkModeToggle(){
-    console.log("toggling dark mode:");
+    console.debug("toggling dark mode:");
     document.body.classList.toggle("dark-mode");
 }
 
 if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-    console.log("setting dark mode");
+    console.debug("setting dark mode");
     document.body.classList.add("dark-mode");
 }
 
