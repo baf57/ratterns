e651f1c67af47b392789272d7a8905d829d9ecb7
Brayden Freitas <baf57@pitt.edu>
2026-06-12 01:40:57 -0400

    NDArray is now a proper object to include dim info and allow new arrays with the same dims to be more easily made

--- js/tools.js
+++ js/tools.js
@@ -1,22 +1,34 @@
 class NDArray{
-    constructor(...dims){
+    array;
+    #dims;
+
+    constructor(fillValue, ...dims){
         /* Create ND array recursively based on dims */
-        function newDim(_i,){
-            if(_i == (dims.length - 1)){
-                // base case
-                return new Array(dims[_i]).fill(0);
-            }
-            
-            // recurse case
-            let _array = new Array(dims[_i]);
-            for(let i=0; i<dims[_i]; i++){
-                _array[i] = newDim(_i+1);
-            }
-            return _array;
+        // ISSUE: this returns and array not an NDArray ??
+
+        this.#dims = dims;
+        this.array = this.#newDim(0, this.#dims, fillValue);
+    }
+
+    get dims(){
+        return this.#dims;
+    }
+
+    newLike(fillValue){
+        return new NDArray(fillValue, ...this.#dims);
+    }
+
+    #newDim(_i, dims, fillValue){
+        if(_i == (dims.length - 1)){
+            // base case
+            return new Array(dims[_i]).fill(fillValue);
         }
-
-        let array = newDim(0);
-
-        return array;
+        
+        // recurse case
+        let _array = new Array(dims[_i]);
+        for(let i=0; i<dims[_i]; i++){
+            _array[i] = this.#newDim(_i+1,dims,fillValue);
+        }
+        return _array;
     }
 }
